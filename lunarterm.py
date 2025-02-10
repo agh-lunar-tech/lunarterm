5a3f1c2e7cb000063be3a3123958686068d9ff23
Y4kCh3ck <j.czekaj@protonmail.com>
2025-02-10 20:46:59 +0100

    Ugly AF but should work for the presentation

--- lunarterm.py
+++ lunarterm.py
@@ -13,13 +13,14 @@ from utils import log
 import socket
 import argparse
 from image import EddieImage
+from frames_proto import lunaris_downlink_pb2
 import json,csv
 import os
 import time as time_lib
 
-DEFAULT_PORT = "/dev/ttyUSB0"
+DEFAULT_PORT = "/dev/ttyUSB1"
 DEFAULT_BAUDRATE = 115200
-FRAME_TIMEOUT = 0.1
+FRAME_TIMEOUT = 0.4
 DEFAULT_MODE = 0 # 0 - everything in everything out, 1 - only frames 
 
 #states:
@@ -37,15 +38,19 @@ class Frame():
     def to_string(self):
         return self.payload.decode('utf-8', errors="ignore")
 
-    def pretty_print(self):
-        sensor_data = lunaris_downlink_pb2.SensorData()
-        sensor_data.ParseFromString(frame.payload)
+    def telemetry_pretty_print(self):
+        pass
+        # f =  b'\x00\x00\x00\x06' + self.payload
+        # print(f)
+        # sensor_data = lunaris_downlink_pb2.SensorData()
+        # try:
+        #     sensor_data.ParseFromString(f)
+        #     print(sensor_data)
 
-        print("Parsed Sensor Data:")
-        print(sensor_data)
-
+        # except Exception as e:
+        #     print("Error during deserialization:", e)
+        
     def telemetry_parse_data(self):
-
         format_str = "<3h 3h h 3i h 6I 2h 2? 3B H"
         field_names = [
             "icm_gyr_data.x", "icm_gyr_data.y", "icm_gyr_data.z",
@@ -56,10 +61,10 @@ class Frame():
             "rdn_temp", "rdn_vdd", "rdn_crystal_ok", "rdn_analog_ok",
             "encoder_sensor", "hall_endstop", "reflective_endstop", "light_sensor"
         ]
-
+        
         unpacked_data = struct.unpack(format_str, self.payload)
         sensor_data = dict(zip(field_names, unpacked_data))
-
+    
         return sensor_data
 
     def telemetry_ugly_print(self):
@@ -71,25 +76,25 @@ class Frame():
     def telemetry_dump_json( self,output_path="log/last_telemetry.json"):
         sensor_data = self.telemetry_parse_data()
         json_data = json.dumps(sensor_data, indent=4)
-
+        
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         with open(output_path, "w") as f:
             f.write(json_data)
-
+        
         return json_data
 
     def telemetry_dump_csv(self, timestamp, output_csv="log/telemetry_data.csv" ):
         sensor_data = self.telemetry_parse_data()
-        sensor_data_with_time = {"time": timestamp, **sensor_data}
+        sensor_data_with_time = {"time": timestamp, **sensor_data}        
         file_exists = os.path.exists(output_csv)
-
+        
         with open(output_csv, mode="a", newline="") as file:
             writer = csv.DictWriter(file, fieldnames=["time"] + list(sensor_data.keys()))
             if not file_exists:
                 writer.writeheader()
-
+            
             writer.writerow(sensor_data_with_time)
-
+                
     def log(self):
         pass
 
@@ -132,7 +137,6 @@ async def eddie_receive(serial):
                 frame.size = frame_sizes[frame.type]
                 state = AWAIT_PAYLOAD
             elif state == AWAIT_PAYLOAD:
-                # print('xd', out)
                 current += 1
                 frame.payload += out
                 if current == frame.size:
